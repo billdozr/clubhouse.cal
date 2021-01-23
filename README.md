87c3acdd7933d7705267a4e495978e3869a1a04c
Andres Riofrio <riofrios@gmail.com>
2021-01-23 18:29:52 -0500

    Insert row in Google Sheets with the event URL

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # clubhouse.cal
 
-To set up, run `yarn`.
-To run, run `yarn start`.
+## Set Up
+
+* Run `yarn`.
+* Create an `.env` file based on `.env.sample`
+* Download a `credentials.json` [from Google](https://developers.google.com/sheets/api/quickstart/nodejs) and move it to this folder. Use https://127.0.0.1/ as the redirect URL.
+
+## Run
+
+`yarn start`
+
+On the first run, when Google Authentication is complete, it will redirect you to `https://127.0.0.1/?code=...&scope=...`. Copy the code from the URL and type it into the console and press Enter.
--- index.ts
+++ index.ts
@@ -1,44 +1,68 @@
-import * as Twitter from "twitter"
-import {google} from "googleapis"
-import * as dotenv from "dotenv"
+import * as fs from "fs";
+import * as dotenv from "dotenv";
 
-dotenv.config()
-const TW_CONSUMER_KEY = process.env.TW_CONSUMER_KEY || "XXXXXXXXXXXXXXXXX"
-const TW_CONSUMER_SEC = process.env.TW_CONSUMER_SEC || "XXXXXXXXXXXXXXXXX"
+import * as Twitter from "twitter";
+import { google } from "googleapis";
+import { authorizeGoogle } from "./google_auth";
+
+dotenv.config();
+const TW_CONSUMER_KEY = process.env.TW_CONSUMER_KEY || "XXXXXXXXXXXXXXXXX";
+const TW_CONSUMER_SEC = process.env.TW_CONSUMER_SEC || "XXXXXXXXXXXXXXXXX";
 const TW_ACCESS_TOKEN_KEY =
-  process.env.TW_ACCESS_TOKEN_KEY || "XXXXXXXXXXXXXXXXX"
+  process.env.TW_ACCESS_TOKEN_KEY || "XXXXXXXXXXXXXXXXX";
 const TW_ACCESS_TOKEN_SEC =
-  process.env.TW_ACCESS_TOKEN_SEC || "XXXXXXXXXXXXXXXXX"
+  process.env.TW_ACCESS_TOKEN_SEC || "XXXXXXXXXXXXXXXXX";
 
 const client = new Twitter({
   consumer_key: TW_CONSUMER_KEY,
   consumer_secret: TW_CONSUMER_SEC,
   access_token_key: TW_ACCESS_TOKEN_KEY,
-  access_token_secret: TW_ACCESS_TOKEN_SEC
-})
+  access_token_secret: TW_ACCESS_TOKEN_SEC,
+});
 
-const sheets = google.sheets('v4')
-// TODO: setup auth
+const sheets = google.sheets("v4");
+fs.readFile("credentials.json", (err, content) => {
+  if (err) return console.log("Error loading client secret file:", err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorizeGoogle(JSON.parse(content.toString()), main);
+});
 
-client.stream(
-  "statuses/filter",
-  {
-    track: "joinclubhouse"
-    //locations: '18.3074488,-34.3583284,19.0046700,-33.4712700'
-  },
-  function(stream) {
-    stream.on("data", function(tweet) {
-      for (const url of tweet.entities.urls) {
-        const expanded_url: String = url.expanded_url
-        const match = expanded_url.match(/^https:\/\/joinclubhouse.com\/event\/(.*)$/)
-        if (match === null) {
-          continue
+async function main(googleAuth) {
+  client.stream(
+    "statuses/filter",
+    {
+      track: "joinclubhouse",
+      //locations: '18.3074488,-34.3583284,19.0046700,-33.4712700'
+    },
+    function (stream) {
+      stream.on("data", async function (tweet) {
+        for (const url of tweet.entities.urls) {
+          const expanded_url: String = url.expanded_url;
+          const match = expanded_url.match(
+            /^https:\/\/joinclubhouse.com\/event\/(.*)$/
+          );
+          if (match === null) {
+            continue;
+          }
+          console.log(expanded_url);
+
+          await sheets.spreadsheets.values.append({
+            spreadsheetId: process.env.GOOGLE_SPREADSHEET_ID,
+            range: "A1",
+            auth: googleAuth,
+            valueInputOption: 'RAW',
+            insertDataOption: 'INSERT_ROWS',
+            requestBody: {
+              values: [
+                [expanded_url]
+              ],
+            }
+          })
         }
-        console.log(expanded_url)
-      }
-    })
-    stream.on("error", function(error) {
-      console.log(error)
-    })
-  }
-)
+      });
+      stream.on("error", function (error) {
+        console.log(error);
+      });
+    }
+  );
+}
